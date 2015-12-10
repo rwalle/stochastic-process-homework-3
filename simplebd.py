65edcef0654dc55a6b1e82dc69fb80324d8a438f
Zhe Li <lizhe05@gmail.com>
2015-12-10 00:20:29 -0500

    finalize

--- simplebd.py
+++ simplebd.py
@@ -6,24 +6,53 @@ from math import log
 class Simplebd:
 
     def __init__(self, kb, kd, t0, t_end, x0):
-        self._kb = kb
-        self._kd = kd
-        self._t0 = t0
+
+        """Assigning values
+
+        kb    : birth rate
+        kd    : death rate
+        t0    : start time
+        t_end : stop time
+        x0    : initial population
+        """
+
+        self._kb    = kb
+        self._kd    = kd
+        self._t0    = t0
         self._t_end = t_end
-        self._x0 = x0
+        self._x0    = x0
 
     def _get_a(self, x):
+
+        """Returning 'a' values
+
+        x: subscript
+        """
+
         a1 = self._kb
         a2 = self._kd * x
-        return [a1+a2, a1, a2]
+        return [a1 + a2, a1, a2]
 
     def _delta_x(self, mu):
+
+        """Decide whether x plus one or minus one based on value of mu
+
+        mu : number of reaction happened
+        """
+
+        if mu != 1 and mu != 2:
+            raise ValueError('Wrong mu value')
+
         if mu == 1:
             return 1
         else:
             return -1
 
     def generate(self):
+
+        """generate a path
+        """
+
         t = self._t0
         x = self._x0
         self._data = []
@@ -32,12 +61,11 @@ class Simplebd:
             n1 = random()
             n2 = random()
 
-            a = self._get_a(x)
-
+            a   = self._get_a(x)
             tau = 1 / a[0] * log(1 / n1)
+            mu  = 0
+            sa  = 0
 
-            mu = 0
-            sa = 0
             while sa < n2 * a[0]:
                 mu += 1
                 sa = sa + a[mu]
@@ -49,13 +77,3 @@ class Simplebd:
 
     def get_data(self):
         return self._data
-
-if __name__ == '__main__':
-    kb = 0.1
-    kd = 0.01
-    x0 = 10
-    t0 = 0
-    t_end = 1000
-    simplebd = Simplebd(kb, kd, t0, t_end, x0)
-    simplebd.generate()
-
--- simplebdstats.py
+++ simplebdstats.py
@@ -2,100 +2,151 @@
 
 from simplebd import Simplebd
 from statistics import mean, variance
-from collections import Counter
 from scipy.stats import poisson, skew
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
-NUMBER_OF_POINTS = 200
-
 class SimplebdStats:
 
-    def __init__(self, kb = 0.2, kd = 0.004, x0 = 3, t0 = 0, t_end = 2000, N = 1000):
+    def __init__(self, kb = 0.2, kd = 0.004, x0 = 1, t0 = 0, t_end = 2000, NUMBER_OF_PATHS = 1000):
+
+        """Assigning values
+        kb: birth rate
+        kd: death rate
+        x0: initial population
+        t0: start time
+        t_end: stop time
+        """
+
         self._kb = kb
         self._kd = kd
         self._t0 = t0
         self._t_end = t_end
         self._x0 = x0
-        self._N = N
-        self._distribution_at_t = {}
 
+        self._NUMBER_OF_PATHS = NUMBER_OF_PATHS
+        self._NUMBER_OF_MOMENTS = 200
 
-        self._collect = []
-        for i in range(N):
+        self._HIST_LAGS = 20
+
+        self._distribution_at_t = {} # used for caching
+
+        self._data = []
+
+        # generate paths
+        for i in range(self._NUMBER_OF_PATHS):
             simplebd = Simplebd(kb, kd, t0, t_end, x0)
             simplebd.generate()
-            self._collect.append(simplebd.get_data())
+            self._data.append(simplebd.get_data())
 
     def find_x_at_t(self, i, t):
+
+        """
+        Given a time t, find out value of x for path i
+        """
+
         if t < self._t0 or t > self._t_end:
-            raise Exception('t is out of range')
+            raise ValueError('t is out of range')
 
-        num = len(self._collect[i])
+        if i < 0 or i > self._NUMBER_OF_PATHS:
+            raise ValueError('i is out of range')
 
-        if t < self._collect[i][0]['t']:
+        num = len(self._data[i])
+
+        if t < self._data[i][0]['t']:
             return self._x0
 
         for j in range(num - 1):
-            if t >= self._collect[i][j]['t'] and t < self._collect[i][j + 1]['t']:
-                return self._collect[i][j]['x']
+            if t >= self._data[i][j]['t'] and t < self._data[i][j + 1]['t']:
+                return self._data[i][j]['x']
 
-        return self._collect[i][num - 1]['x']
-
-    def get_first_passage(self, theta):
-
-        c = []
-
-        for i in range(self._N):
-            num = len(self._collect[i])
-
-            for j in range(num):
-                if self._collect[i][j]['x'] >= theta:
-                    c.append(self._collect[i][j]['t'])
-                    break
-
-        return c
-
-
-
-
+        return self._data[i][num - 1]['x']
 
     def get_distribution_at_t(self, t):
+
+        """
+        Return a collection of values of x at time t
+        """
+
+        if t < self._t0 or t > self._t_end:
+            raise ValueError('t is out of range')
+
+        #_distribution_at_t seves as cache
         if t in self._distribution_at_t:
             return self._distribution_at_t[t]
         else:
             x = []
-            for i in range(self._N):
+            for i in range(self._NUMBER_OF_PATHS):
                 x.append(self.find_x_at_t(i, t))
             self._distribution_at_t[t] = x
             return x
 
     def get_lambda(self, t):
+
+        """
+        Return the value of lambda at given time t
+        """
+
         return self._kb / self._kd * (1 - np.exp(- self._kd * t))
 
     def get_probability(self, x, t):
+
+        """
+        Return the probability P(x, t)
+        """
+
         lambda_ = self.get_lambda(t)
         return lambda_ ** x / np.math.factorial(x) * np.exp(- lambda_)
 
     def poisson_distribution(self, lambda_, x):
+
+        """
+        Return value of poisson distribution at x with lambda = lambda_
+        """
+
         return poisson.pmf(x, lambda_)
 
-    def get_analytical_corr_at_t(self, t, tau):
+    def get_analytical_correlation_at_t(self, t, tau):
+
+        """
+        Return analytical correlation of time t and t + tau
+        """
+
         return self.get_analytical_variance_at_t(t) * np.exp(-self._kd * tau)
 
     def get_analytical_mean_at_t(self, t):
+
+        """
+        Return analytical ensemble mean at time t
+        """
+
         return self._kb / self._kd + (self._x0 - self._kb / self._kd) * np.exp(- self._kd * t)
 
     def get_analytical_variance_at_t(self, t):
-        return -self._x0 * np.exp(-2*self._kd*t) + self._kb / self._kd * (1 - np.exp(-self._kd*t)) + np.exp(-self._kd*t)*self._x0
+
+        """
+         Return analytical ensemble variance at time t
+        """
+
+        return -self._x0 * np.exp(-2 * self._kd * t) + self._kb / self._kd * (1 - np.exp(-self._kd * t)) + np.exp(-self._kd * t) * self._x0
 
     def get_analytical_x2_at_t(self, t):
+
+        """
+        Return analytical <x^2> at time t
+        """
+
         return np.exp(- 2 * self._kd * t) * (self._kb ** 2 / self._kd ** 2 - 2 * self._kb / self._kd * self._x0 - self._x0 + self._x0 ** 2) + \
                 np.exp(- self._kd * t) * (- 2 * self._kb ** 2 / self._kd ** 2 - self._kb / self._kd + 2 * self._kb * self._x0 / self._kd + self._x0) + \
                 1 / self._kd ** 2 * (self._kb ** 2 + self._kb * self._kd)
 
     def get_analytical_x3_at_t(self, t):
+
+        """
+        Return analytical <x^3> at time t
+        """
+
         kb = self._kb
         kd = self._kd
         x0 = self._x0
@@ -105,12 +156,27 @@ class SimplebdStats:
                 1 / kd ** 3 * (3 * kb ** 2 * kd + kb ** 3 + kb * kd ** 2)
 
     def get_analytical_skewness_at_t(self, t):
+
+        """
+        Return analytical skewness at time t
+        """
+
         return (self.get_analytical_x3_at_t(t) - 3 * self.get_analytical_mean_at_t(t) * self.get_analytical_variance_at_t(t) - self.get_analytical_mean_at_t(t) ** 3) / self.get_analytical_variance_at_t(t) ** 1.5
 
     def get_simulative_x2_at_t(self, a):
+
+        """
+        Return simulative <x^2> at time t
+        """
+
         return np.mean(np.array(a) ** 2)
 
     def get_simulative_x3_at_t(self, a):
+
+        """
+        Return simulative <x^3> at time t
+        """
+
         return np.mean(np.array(a) ** 3)
 
     def get_analytical_statistics(self, analytical_function):
@@ -119,25 +185,38 @@ class SimplebdStats:
 
         return x
 
-    def get_simulative_statistics(self, stochastic_statistics_function):
+    def get_simulative_statistics(self, simulative_statistics_function):
 
-        x = np.vectorize(lambda v: stochastic_statistics_function(self.get_distribution_at_t(v)))(self.t)
+        x = np.vectorize(lambda v: simulative_statistics_function(self.get_distribution_at_t(v)))(self.t)
 
         return x
 
-    def get_statistics(self):
+    def single_path(self):
+        data = self._data[0]
 
-        self.t = np.linspace(self._t0, self._t_end, NUMBER_OF_POINTS)
+        t = []
+        x = []
 
-        self.x_max_stochastic       = self.get_stochastic_statistics(max)
-        self.x_min_stochastic       = self.get_stochastic_statistics(min)
-        self.x_mean_analytical      = self.get_analytical_statistics(self.get_analytical_mean_at_t)
-        self.x_mean_stochastic      = self.get_stochastic_statistics(mean)
-        self.x_variance_stochastic  = self.get_stochastic_statistics(variance)
-        self.x_variance_analytical  = self.get_analytical_statistics(self.get_analytical_variance_at_t)
+        for i in range(len(data)):
+            t.append(data[i]['t'])
+            x.append(data[i]['x'])
+
+        plt.plot(t, x)
+        plt.xlabel("t")
+        plt.ylabel("x")
+        plt.show(block = False)
+
+
 
     def compare(self, analytical_function, simulative_function, ylabel_text):
-        self.t = np.linspace(self._t0, self._t_end, NUMBER_OF_POINTS)
+
+        """
+        Compare analytical and simulative result defined by function analytical_function and simulative_function at time t
+        and plot figure
+        """
+
+        self.t = np.linspace(self._t0, self._t_end, self._NUMBER_OF_PATHS)
+
         x_mean_analytical = self.get_analytical_statistics(analytical_function)
         x_mean_simulative = self.get_simulative_statistics(simulative_function)
         plt.plot(self.t, x_mean_analytical, color = 'blue')
@@ -165,41 +244,71 @@ class SimplebdStats:
         return self.compare(self.get_analytical_x3_at_t, self.get_simulative_x3_at_t, 'x3')
 
     def compare_distribution(self):
-        number_of_snapshots = 4
+        number_of_snapshots = 5
 
         snapshots = np.linspace(self._t0 + (self._t_end - self._t0) / number_of_snapshots,
                 self._t_end, number_of_snapshots)
 
         for t in snapshots:
-            stochastic_distribution = np.sort(self.get_distribution_at_t(t))
-            count = Counter(stochastic_distribution)
-            x_stochastic = np.asarray(list(count.keys()))
-            p_stochastic = np.vectorize(lambda x: x / self._N)(list(count.values()))
-            x_analytical = np.arange(min(x_stochastic), max(x_stochastic), 1)
+
+            simulative_distribution = np.sort(self.get_distribution_at_t(t))
+            x_analytical = np.arange(min(simulative_distribution), max(simulative_distribution), 1)
             p_analytical = np.vectorize(self.poisson_distribution)(self.get_lambda(t),
                     x_analytical)
-            plt.plot(x_stochastic, p_stochastic)
-            plt.plot(x_analytical, p_analytical)
-            plt.xlabel("t")
-            plt.ylabel("x")
+
+            # plot
+            plt.hist(simulative_distribution, self._HIST_LAGS, normed = True, color = 'red')
+            plt.plot(x_analytical, p_analytical, color = 'blue')
+
+            red = mpatches.Patch(color = 'red', label = 'Simulation')
+            blue = mpatches.Patch(color = 'blue', label = 'Analytical Prediction')
+            plt.legend(handles = [blue, red])
+
+            plt.xlabel("x")
+            plt.ylabel("p")
+            plt.title("t = " + str(t))
             plt.show()
 
-    def correlation(self):
+    def compare_time_average(self):
+
+        tau = 1 / self._kd
+        t1 = tau * 6
+        t2 = tau * 205
+        single_path = SimplebdStats(kb = self._kb, kd = self._kd, x0 = self._x0, t0 = self._t0, t_end = t2, NUMBER_OF_PATHS = 1)
+        t = np.linspace(t1, t2, 200)
+        x_single_path = np.vectorize(single_path.find_x_at_t)(0, t)
+        x_time_average = np.mean(x_single_path)
+        x_ensemble = self.get_distribution_at_t(t1)
+        x_ensemble_average = np.mean(x_ensemble)
+        print("Time Average is: " + str(x_time_average))
+        print("Ensemble Average is: " + str(x_ensemble_average))
+        print("\n")
+
+
+    def compare_autocorrelation(self):
+
         t_start = self._t0 + (self._t_end - self._t0) / 4
         t_end = self._t0 + (self._t_end - self._t0) * 3 / 4
         dis_0 = self.get_distribution_at_t(t_start)
-        t = np.linspace(t_start, t_end, NUMBER_OF_POINTS)
-        corr_stochastic = np.vectorize(lambda x: np.cov(dis_0, self.get_distribution_at_t(x))[0][1])(t)
-        corr_analytical = np.vectorize(self.get_analytical_corr_at_t)(t_start, t - t_start)
-        #print(t)
-        #print(corr_stochastic)
-        #print(corr_analytical)
-        plt.plot(t, corr_stochastic)
-        plt.plot(t, corr_analytical)
+        t = np.linspace(t_start, t_end, self._NUMBER_OF_MOMENTS)
+
+        corr_simulative = np.vectorize(lambda x: np.cov(dis_0, self.get_distribution_at_t(x))[0][1])(t)
+        corr_analytical = np.vectorize(self.get_analytical_correlation_at_t)(t_start, t - t_start)
+
+        # plot
+        plt.plot(t - t_start, corr_simulative, color = 'red')
+        plt.plot(t - t_start, corr_analytical, color = 'blue')
+        red = mpatches.Patch(color = 'red', label = 'Simulation')
+        blue = mpatches.Patch(color = 'blue', label = 'Analytical Prediction')
+        plt.legend(handles = [blue, red])
+        plt.xlabel(r'$\tau$')
+        plt.ylabel("autocorrelation")
+
         plt.show()
 
-    def get_analytical_first_passage_dist(self, theta = 45):
-        t = np.linspace(self._t0, self._t_end, self._N)
+    def get_analytical_first_passage_distribution(self, theta = 45):
+
+        t = np.linspace(self._t0, self._t_end, self._NUMBER_OF_MOMENTS)
         p0 = []
         for t0 in t:
             s = 0
@@ -209,6 +318,7 @@ class SimplebdStats:
         t_axis = []
         p_axis = []
 
+        # use difference as differential. f'(x) = (f(x+dx)-f(x-dx))/(2dx)
         for idx in range(1, len(t) - 2):
             t_axis.append(t[idx])
             dp = - (p0[idx + 1] - p0[idx - 1]) / (t[idx + 1] - t[idx - 1])
@@ -216,22 +326,35 @@ class SimplebdStats:
 
         return t_axis, p_axis
 
+    def get_simulative_first_passage_distribution(self, theta):
 
+        c = []
 
+        for i in range(self._NUMBER_OF_PATHS):
+            num = len(self._data[i])
 
+            for j in range(num):
+                if self._data[i][j]['x'] >= theta:
+                    c.append(self._data[i][j]['t'])
+                    break
 
+        return c
 
     def compare_first_passage_dist(self, theta = 45):
-        dist = self.get_first_passage(theta)
 
-        #self.simulative_distribution = np.sort(dist)
-        plt.hist(dist, 20, normed = True)
-#        self.count = Counter(self.simulative_distribution)
-#        self.x_simulative = np.asarray(list(self.count.keys()))
-#        self.p_simulative = np.vectorize(lambda x: x / self._N)(list(self.count.values()))
-        x_analytical, p_analytical = self.get_analytical_first_passage_dist()
-#        plt.plot(self.x_simulative, self.p_simulative)
-        plt.plot(x_analytical, p_analytical)
+        """
+        theta is the threshold
+        """
+
+        dist = self.get_simulative_first_passage_distribution(theta)
+
+        x_analytical, p_analytical = self.get_analytical_first_passage_distribution()
+
+        plt.hist(dist, self._HIST_LAGS, normed = True, color = 'red')
+        plt.plot(x_analytical, p_analytical, color = 'blue')
+        red = mpatches.Patch(color = 'red', label = 'Simulation')
+        blue = mpatches.Patch(color = 'blue', label = 'Analytical Prediction')
+        plt.legend(handles = [blue, red])
         plt.show()
 
 
@@ -241,10 +364,30 @@ class SimplebdStats:
 
 if __name__ == '__main__':
 
-    simplebdStats = SimplebdStats()
-    simplebdStats.get_statistics()
+    simplebdStats = SimplebdStats(kb = 0.2, kd = 0.004, x0 = 2, t0 = 0, t_end = 2000)
 
+    input("0. A Single Path. Press enter to continue\n")
+    simplebdStats.single_path()
 
+    input("3. Comparison between analytical and simulative result for mean. Press enter to continue\n")
+    simplebdStats.compare_mean()
 
+    input("3. Comparison between analytical and simulative result for variance. Press enter to continue\n")
+    simplebdStats.compare_variance()
 
+    input("3. Comparison between analytical and simulative result for skewness. Press enter to continue\n")
+    simplebdStats.compare_skewness()
+
+    input("4. Comparison between analytical and simulative result for distribution at 5 different times. Press enter to continue\n")
+    simplebdStats.compare_distribution()
+
+    input("5. Time average equals ensemble average. Press enter to continue.")
+    simplebdStats.compare_time_average()
+
+    t_start = simplebdStats._t0 + (simplebdStats._t_end - simplebdStats._t0) / 4
+    input("6. Comparison between analytical and simulative result for correlation function C(t, t + tau) where t = " + str(t_start) + ". Press enter to continue\n")
+    simplebdStats.compare_autocorrelation()
+
+    input("7. Comparison between analytical and simulative result for first time passage at threshold 45. Press enter to continue")
+    simplebdStats.compare_first_passage_dist()
 
